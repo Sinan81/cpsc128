8412cbf5221c9eea9ea7b1e2f07107cdbb85194d
Sinan Bulut <sbulut@yukoncollege.yk.ca>
2019-05-09 19:07:40 -0700

    circumference to diameter example

---
+++ code/python3/circ2diam.py
@@ -0,0 +1,22 @@
+"""
+# -*- coding: utf-8 -*-
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import math
+import math as m
+from math import pi
+#from math import *
+
+circ = eval(input("Enter circumference:"))
+print()
+diameter = circ/math.pi
+diameter2 = circ/m.pi
+diameter3 = circ/pi
+
+print("Diamter is ",diameter)
+print("Diamter is : %1.2f" % diameter)
+print("Diamter is : %1.2f" % diameter2)
+print("Diamter is : %1.2f" % diameter3)
