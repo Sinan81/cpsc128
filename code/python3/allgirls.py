6763d1702bec62d10937f8f6105ec2a3df128752
github.com/Sinan81 <Sinan81@earth>
2019-05-20 20:01:59 -0700

    code for May21 class

---
+++ code/python3/allgirls.py
@@ -0,0 +1,20 @@
+# allgirsl.py
+
+# simulate 1000 4-child families
+# count how many of them have all-daughters
+import random
+
+allgirls = 0
+for family in range(1000):
+
+    daughters = 0
+    for birth in range(4):
+        # assign 0 -> a boy, 1-> a girl
+        if random.randint(0,1) == 1:
+            daughters = daughters + 1
+
+    if daughters == 4:
+        allgirls = allgirls + 1
+
+        
+print("Percentage of all girl families is: %1.2f" % (allgirls/1000.*100) )
