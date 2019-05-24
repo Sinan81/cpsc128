2d2d25064203999a2e5106c52d5eda425360b18d
github.com/Sinan81 <Sinan81@earth>
2019-05-24 00:55:09 -0700

    minor updates

--- code/python3/dice_roll_v2.py
+++ code/python3/dice_roll_v2.py
@@ -1,8 +1,8 @@
 import random
-import time as tm
-start = tm.time()
+#import time as tm
+#start = tm.time()
 
-ROLLS = 10**6
+ROLLS = 1000
 
 # Initialize counters to 0.
 counters = [0] * 11
@@ -11,7 +11,6 @@ counters = [0] * 11
 for roll in range(ROLLS):
     outcome = random.randint(1, 6) + random.randint(1, 6)
     # Now increment the appropriate counter.
-    # Notice that no testing is needed because we can calculate the index of the matching entry.
     counters[outcome-2] = counters[outcome-2] + 1
         
 # Display results.
@@ -22,5 +21,5 @@ for posn in range(11):
     print("%7d   |%8d" % (posn+2, counters[posn]))
 print("  ---------------------")
 
-end = tm.time()
-print("Calculation completed in ",end-start," seconds")
+#end = tm.time()
+#print("Calculation completed in ",end-start," seconds")
